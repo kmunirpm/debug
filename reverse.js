53b81dd330bf64ca1b1e9e124500ceb882a13ac9
kmunirpm <km092009@gmail.com>
2021-10-27 01:37:03 +0000

    reverse.js file fixed

--- reverse.js
+++ reverse.js
@@ -4,5 +4,7 @@ if (input)
   console.log(reverse(input));
 
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  return original.split('').reverse().join('');
 }
+
+console.log(reverse("Hello"))
